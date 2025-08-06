00095f8a6ab7571c4207523adc2e3c230b7df485
Clark Lawrence Ching <Rencechichi@gmail.com>
2025-08-06 18:09:01 +0800

    Update terminal_game.py

---
+++ Project/terminal_game.py
@@ -0,0 +1,4 @@
+input("Welcome Lost Explorer to the Jungle of Mystara")
+input("You are an intrepid explorer venturing into the uncharted Jungle of Mystara\n"
+      "Your journey is fraught with danger—wild beasts, treacherous terrain, and ancient traps guard the prize.\n"
+      "Every decision shapes your fate: will you find the Crystal, or be lost forever in the jungle’s embrace?")
